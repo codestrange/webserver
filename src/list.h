2e7e153f061ac3b6d9f89bc5b1f5e34c82efd2ff
leynier <leynier41@gmail.com>
2018-11-26 08:08:48 -0500

    Implementar listas de char* y char**

---
+++ src/list.h
@@ -0,0 +1,56 @@
+#ifndef LIST
+#define LIST
+
+typedef struct CharList {
+    char *array;
+    int size;
+    int capacity;
+} CharList;
+
+typedef struct CharCharList {
+    CharList *array;
+    int size;
+    int capacity;
+} CharCharList;
+
+CharList new_charlist(int capacity);
+
+void insert_charlist(CharList *list, int index, char item);
+
+void append_charlist(CharList *list, char item);
+
+void clear_charlist(CharList *list);
+
+char remove_charlist(CharList *list, int index);
+
+char pop_charlist(CharList *list);
+
+char index_charlist(CharList *list, int index);
+
+void free_charList(CharList *list);
+
+CharCharList new_charcharlist(int capacity);
+
+void insert_charcharlist(CharCharList *list, int index, CharList item);
+
+void append_charcharlist(CharCharList *list, CharList item);
+
+void clear_charcharlist(CharCharList *list);
+
+CharList remove_charcharlist(CharCharList *list, int index);
+
+CharList pop_charcharlist(CharCharList *list);
+
+CharList index_charcharlist(CharCharList *list, int index);
+
+void free_charcharList(CharCharList *list);
+
+char *convert_arraychar(CharList *charList);
+
+char **convert_arraycharchar(CharCharList *charCharList);
+
+CharList convert_CharList(char *str);
+
+CharCharList convert_CharCharList(char **str);
+
+#endif
