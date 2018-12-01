ca6644b9cbb6b595e4e63720809bd597992e9416
leynier <leynier41@gmail.com>
2018-12-01 09:02:21 -0500

    Cambiar la identacion de 2 espacios a 4

--- src/server.c
+++ src/server.c
@@ -23,43 +23,42 @@ int main(int argc, char const *argv[]) {
     char buf[BUFF_SIZE];
     ClientList clients = new_clientlist(1024);
     /*Fin de la inicialización*/
-
     while (true) {
-      /*Iniciar conjunto del Poll*/
-      struct pollfd *fds = malloc((clients.size + 2) * sizeof(struct pollfd));
-      fds[0].fd = listenfd;
-      fds[0].events = POLLIN;
-      fds[0].revents = 0;
-      for(int i = 0; i < clients.size; ++i) {
-        fds[i + 1].fd = index_clientlist(&clients, i).fd;
-        fds[i + 1].events = POLLIN | POLLOUT;
-        fds[i + 1].revents = 0;
-      }
-      /*Ejecutando poll*/
-      poll(fds, clients.size + 1, -1);
-      /*Ver nuevas conexiones*/
-      if (fds[0].revents & POLLIN) {
-        Client newclient = get_client_fd(listenfd);
-        append_clientlist(&clients, newclient);
-      }
-      for (int i = 0; i < clients.size; ++i) {
-        if ((fds[i + 1].revents & POLLIN) && (fds[i + 1].revents & POLLOUT)) {
-          bzero(buf, BUFF_SIZE);
-          if (read(fds[i + 1].fd, buf, BUFF_SIZE) > 0) {
-            Request request = parse_request(buf);
-            printf("Petición a la url: %s\n", request.url);
-            char *dir = malloc(BUFF_SIZE * sizeof(char));
-            getcwd(dir, BUFF_SIZE);
-            char *response = get_response(dir, request.url);
-            write(fds[i + 1].fd, response, strlen(response));
-            free(response); 
-          }
-          close(fds[i + 1].fd);
-          remove_clientlist(&clients, i);
+        /*Iniciar conjunto del Poll*/
+        struct pollfd *fds = malloc((clients.size + 2) * sizeof(struct pollfd));
+        fds[0].fd = listenfd;
+        fds[0].events = POLLIN;
+        fds[0].revents = 0;
+        for(int i = 0; i < clients.size; ++i) {
+            fds[i + 1].fd = index_clientlist(&clients, i).fd;
+            fds[i + 1].events = POLLIN | POLLOUT;
+            fds[i + 1].revents = 0;
         }
-      }
-      /*Liberar conjunto viejo*/
-      free(fds);
+        /*Ejecutando poll*/
+        poll(fds, clients.size + 1, -1);
+        /*Ver nuevas conexiones*/
+        if (fds[0].revents & POLLIN) {
+            Client newclient = get_client_fd(listenfd);
+            append_clientlist(&clients, newclient);
+        }
+        for (int i = 0; i < clients.size; ++i) {
+            if ((fds[i + 1].revents & POLLIN) && (fds[i + 1].revents & POLLOUT)) {
+            bzero(buf, BUFF_SIZE);
+            if (read(fds[i + 1].fd, buf, BUFF_SIZE) > 0) {
+                Request request = parse_request(buf);
+                printf("Petición a la url: %s\n", request.url);
+                char *dir = malloc(BUFF_SIZE * sizeof(char));
+                getcwd(dir, BUFF_SIZE);
+                char *response = get_response(dir, request.url);
+                write(fds[i + 1].fd, response, strlen(response));
+                free(response); 
+            }
+            close(fds[i + 1].fd);
+            remove_clientlist(&clients, i);
+            }
+        }
+        /*Liberar conjunto viejo*/
+        free(fds);
     }
     /*Fin de Conexión*/  
     close(listenfd);
