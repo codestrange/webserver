7535f3ec648191d2b2d22bae6ddcd0570b95db8a
leynier <leynier41@gmail.com>
2018-12-01 08:59:59 -0500

    Reformatear server.c

--- src/server.c
+++ src/server.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <poll.h>
+#include <stdbool.h>
 #include "connection.h"
 #include "parser.h"
 #include "files.h"
@@ -23,37 +24,37 @@ int main(int argc, char const *argv[]) {
     ClientList clients = new_clientlist(1024);
     /*Fin de la inicialización*/
 
-    while(1) {
+    while (true) {
       /*Iniciar conjunto del Poll*/
       struct pollfd *fds = malloc((clients.size + 2) * sizeof(struct pollfd));
       fds[0].fd = listenfd;
       fds[0].events = POLLIN;
       fds[0].revents = 0;
       for(int i = 0; i < clients.size; ++i) {
-        fds[i+1].fd = index_clientlist(&clients, i).fd;
-        fds[i+1].events = POLLIN | POLLOUT;
-        fds[i+1].revents = 0;
+        fds[i + 1].fd = index_clientlist(&clients, i).fd;
+        fds[i + 1].events = POLLIN | POLLOUT;
+        fds[i + 1].revents = 0;
       }
       /*Ejecutando poll*/
       poll(fds, clients.size + 1, -1);
       /*Ver nuevas conexiones*/
-      if(fds[0].revents & POLLIN) {
+      if (fds[0].revents & POLLIN) {
         Client newclient = get_client_fd(listenfd);
         append_clientlist(&clients, newclient);
       }
-      for(int i=0; i < clients.size; ++i) {
-        if((fds[i+1].revents & POLLIN) && (fds[i+1].revents & POLLOUT)) {
+      for (int i = 0; i < clients.size; ++i) {
+        if ((fds[i + 1].revents & POLLIN) && (fds[i + 1].revents & POLLOUT)) {
           bzero(buf, BUFF_SIZE);
-          if( read(fds[i+1].fd, buf, BUFF_SIZE) > 0) {
+          if (read(fds[i + 1].fd, buf, BUFF_SIZE) > 0) {
             Request request = parse_request(buf);
             printf("Petición a la url: %s\n", request.url);
             char *dir = malloc(BUFF_SIZE * sizeof(char));
             getcwd(dir, BUFF_SIZE);
             char *response = get_response(dir, request.url);
-            write(fds[i+1].fd, response, strlen(response));
+            write(fds[i + 1].fd, response, strlen(response));
             free(response); 
           }
-          close(fds[i+1].fd);
+          close(fds[i + 1].fd);
           remove_clientlist(&clients, i);
         }
       }
